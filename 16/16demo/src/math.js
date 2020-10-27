002d25c122d137bc254b91f981aa2407672b3ab9
Andy tseng <txshon@Andys-MacBook-Pro.local>
2020-10-27 09:50:17 +0800

    npm init and add math.js

---
+++ 16/16demo/src/math.js
@@ -0,0 +1,13 @@
+const PI = 3.14
+export { PI };
+export const limit = 18
+
+function add(){}
+export { add }
+export function square(){}
+
+//
+export default PI = 3.14
+// export default function add(){
+
+// }
